161a9d7ca2db6537ccd1d57b3b474e5f5b7fc918
Michael Green <m_green@hotmail.co.nz>
2025-06-22 16:49:58 +1000

    allow user to change drone mass

--- dashboard.py
+++ dashboard.py
@@ -44,6 +44,10 @@ kp = st.sidebar.slider("Kp (Proportional)", 0.0, KP * 2, KP, 0.05)
 ki = st.sidebar.slider("Ki (Integral)", 0.0, KI * 2, KI, 0.01)
 kd = st.sidebar.slider("Kd (Derivative)", 0.0, KD * 2, KD, 0.05)
 
+st.sidebar.header("Drone Properties")
+drone_mass = st.sidebar.slider(
+    "Drone Mass (kg)", 0.1, float(DRONE_MASS * 2), float(DRONE_MASS), 0.1
+)
 
 logger.info("Setting up simulator...")
 time_points = np.arange(0, TOTAL_SIM_TIME, TIME_STEP)
@@ -52,7 +56,7 @@ simulator = Simulator(
     ki=ki,
     kd=kd,
     time_points=time_points,
-    drone_mass=DRONE_MASS,
+    drone_mass=drone_mass,
     desired_position=DESIRED_POSITION,
     initial_position=INITIAL_POSITION,
     external_force_vector=EXTERNAL_FORCE_VECTOR,
